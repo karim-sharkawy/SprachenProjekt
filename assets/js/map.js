a8eb6e2626a7ea717380926f37efc11a17f1a1ef
Karim El-Sharkawy <107355198+karim-sharkawy@users.noreply.github.com>
2024-06-09 03:46:50 -0400

    Update map.js

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,3 +1,84 @@
+/* Imports */
+import * as am5 from "@amcharts/amcharts5";
+import * as am5map from "@amcharts/amcharts5/map";
+import am4geodata_worldLow from "@amcharts/amcharts4-geodata/worldLow";
+import am5themes_Animated from "@amcharts/amcharts5/themes/Animated";
+
+/* Chart code */
+// Create root element
+let root = am5.Root.new("chartdiv");
+
+// Set themes
+root.setThemes([
+  am5themes_Animated.new(root)
+]);
+
+// Create the map chart
+let chart = root.container.children.push(am5map.MapChart.new(root, {
+  panX: "rotateX",
+  panY: "rotateY",
+  projection: am5map.geoOrthographic(),
+  paddingBottom: 20,
+  paddingTop: 20,
+  paddingLeft: 20,
+  paddingRight: 20
+}));
+
+// Create main polygon series for countries
+let polygonSeries = chart.series.push(am5map.MapPolygonSeries.new(root, {
+  geoJSON: am4geodata_worldLow 
+}));
+
+polygonSeries.mapPolygons.template.setAll({
+  tooltipText: "{name}",
+  toggleKey: "active",
+  interactive: true
+});
+
+polygonSeries.mapPolygons.template.states.create("hover", {
+  fill: root.interfaceColors.get("primaryButtonHover")
+});
+
+polygonSeries.mapPolygons.template.states.create("active", {
+  fill: root.interfaceColors.get("primaryButtonHover")
+});
+
+// Create series for background fill
+let backgroundSeries = chart.series.push(am5map.MapPolygonSeries.new(root, {}));
+backgroundSeries.mapPolygons.template.setAll({
+  fill: root.interfaceColors.get("alternativeBackground"),
+  fillOpacity: 0.1,
+  strokeOpacity: 0
+});
+backgroundSeries.data.push({
+  geometry: am5map.getGeoRectangle(90, 180, -90, -180)
+});
+
+let graticuleSeries = chart.series.unshift(
+  am5map.GraticuleSeries.new(root, {
+    step: 10
+  })
+);
+
+graticuleSeries.mapLines.template.set("strokeOpacity", 0.1);
+
+// Set up events
+let previousPolygon;
+
+polygonSeries.mapPolygons.template.on("active", function(active, target) {
+  if (previousPolygon && previousPolygon != target) {
+    previousPolygon.set("active", false);
+  }
+  if (target.get("active")) {
+    chart.zoomToDataItem(target.dataItem);
+  } else {
+    chart.goHome();
+  }
+  previousPolygon = target;
+});
+
+chart.appear(1000, 100);
+
 am5.ready(function() {
 
     // Create root and chart
